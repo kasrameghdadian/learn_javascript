20e17e8b72bf62caa0c061726d721d9381fe4391
Programmer-X <Programmer-X@MOM.LOCAL>
2025-04-27 19:09:08 +0330

    solve second problem

---
+++ 2.js
@@ -0,0 +1,12 @@
+/*
+2. Check if a Number is Even or Odd
+Problem: Write a program that checks whether a given number is even or odd and prints the result.
+*/
+
+let number = 3;
+
+if (number % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
