0c5c5f45fed77313c450b1e8c03c57d6c51d1531
kasra.nz1385@gmail.com <kasra.nz1385@gmail.com>
2025-06-07 17:14:31 +0330

    loop

--- new/index.js
+++ new/index.js
@@ -1,74 +1,8 @@
-// let input = ["h","o","l","a"]
-// let result = ""
-// for(let i = 0;i < input.length;i++){
-//     result =  result + input[i] + ','
-// }
+let numbers = [3, 7, 2, 8, 5];
+let sum = 0;
 
-// console.log(result)
-
-// console.log(result)
-// let result = ''
-// console.log(typeof result)
-// for(let i = 0; i < input.length; i++){
-//     console.log(typeof input[i])
-//     input[i] + result 
-// }
-
-// console.log(result)
-
-
-
-
-// function arrowUp(){
-//     return 'up'
-// }
-
-// function arrowDown(){
-//     return 'down'
-// }
-
-// function arrowLeft(){
-//     return 'left'
-// }
-
-// function arrowRight(){
-//     return 'right'
-// }
-
-
-// console.log(arrowDown());
-
-//let xArr = ["A" , 0 , "A" , 1 , "B" , 12]
-
-
-
-//1. Easy – Sum of All Elements
-//Problem: Given an array of numbers, return the sum of all elements.
-
-function sumArray(arr) {
-    let sum = 0; 
-    for (let i = 0; i < arr.length; i++) { 
-        sum += arr[i]; 
-    }
-    return sum; 
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]; 
 }
 
-console.log(sumArray([1, 2, 3, 4])); 
-
-
-
-
-//2. Medium – Remove Duplicates
-//Problem: Write a function that removes duplicates from an array.
-
-function removeDuplicates(arr) {
-    let uniqueArr = []; 
-    for (let i = 0; i < arr.length; i++) { 
-        if (uniqueArr.indexOf(arr[i]) === -1) { 
-            uniqueArr.push(arr[i]); 
-        }
-    }
-    return uniqueArr; 
-}
-
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
+console.log("Sum of numbers: " + sum);
